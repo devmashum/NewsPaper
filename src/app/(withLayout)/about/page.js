a57e04322361dd1494a6ca510c7504b2f32ddec0
devmashum <dev.mashum@gmail.com>
2023-12-06 00:10:55 +0100

    learnd link and image component

--- src/app/(withLayout)/about/page.js
+++ src/app/(withLayout)/about/page.js
@@ -1,6 +1,11 @@
 import React from 'react';
+export const metadata={
+    title: "About Page",
+    description: "This is about page"
+};
 
 const AboutPage = () => {
+    
     return (
         <div>
             <h1>About Page with header and footer</h1>
